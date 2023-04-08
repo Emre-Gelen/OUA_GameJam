3e5e012bc5affe491b099ed604b83ef2eff5bdbb
Emirhan Aydın Çakıl <emir76aydin44@gmail.com>
2023-04-09 00:27:11 +0300

    Deneme1

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // aydin
     public float MovementSpeed = 7f;
     public float JumpForce = 13f;
     public float GroundCheckRadius;
